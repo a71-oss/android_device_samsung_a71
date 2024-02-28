a0ece21077bc341097607931310bccd607414158
Jesse Chan <jc@lineageos.org>
2024-02-28 13:40:28 +0100

    a71: overlay: Switch to Snap camera
    Change-Id: Ie8742d8ec400414852fa99e945c69dcfc909b0cb

--- device.mk
+++ device.mk
@@ -95,6 +95,7 @@ PRODUCT_PACKAGES += \
     camera.device@3.3-impl \
     camera.device@3.4-impl \
     camera.device@3.5-impl \
+    Snap \
 
 # Charger
 PRODUCT_PACKAGES += \
@@ -219,6 +220,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(DEVICE_PATH)/overlay-lineage/lineage-sdk \
+    $(DEVICE_PATH)/overlay-lineage/packages/apps/Snap \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
